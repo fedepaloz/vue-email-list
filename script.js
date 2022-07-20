da85b67a84711c4594e953c635d9e4c120b8edf5
fedepaloz <palozzifederico@gmail.com>
2022-07-20 17:27:44 +0200

    getrandommail

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const root = new Vue ({
-    el : '#root' ,
-    data : {},
-    methods : {},
+console.log('Vue ok', Vue)
+const root = new Vue({
+    el: '#root',
+    data: {
+        items:5,
+        mails: []
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+
+            this.mails = res.data.response;
+
+        })
+    },
+    methods: {
+        getRandomMails() {
+            for (let i = 0; i < items; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    const randomMails = res.data.response;
+                    this.mails.push(randomMails)
+
+                })
+
+
+
+            }
+        }
+
+
+    },
+
 })
 
